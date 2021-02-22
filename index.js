d03d30320d82edb901ecd8d12c92cc1694d7233a
Artem Smirnov <aratatem2013@gmail>
2021-02-22 15:53:12 +0200

    деякі зміни

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import cardTpl from './templates/card.hbs';
 import menu from './menu.json';
 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const bodyRef = document.querySelector('body');
+console.log(bodyRef);
+const checkboxRef = document.querySelector('#theme-switch-toggle');
 const menuContainer = document.querySelector('.js-menu');
 const cardsMarkup = createCardsMarkup(menu);
-console.log(menu);
 
 menuContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+checkboxRef.addEventListener('change', onThemeChange);
+
+function onThemeChange() {
+  // bodyRef.classlist.add(Theme.DARK);
+}
 
 function createCardsMarkup(menu) {
   return menu.map(cardTpl).join('');
