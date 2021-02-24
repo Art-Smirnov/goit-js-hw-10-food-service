9ec001edde3c7b02b6f42ffbacf38034cd541e0f
Artem Smirnov <aratatem2013@gmail>
2021-02-24 13:45:39 +0200

    фікси

--- index.js
+++ index.js
@@ -41,20 +41,9 @@ function populateBodyClassList() {
 }
 
 function load(key) {
-  try {
-    const serializedState = localStorage.getItem(key);
-
-    return serializedState === null ? undefined : JSON.parse(serializedState);
-  } catch (err) {
-    console.error('Get state error: ', err);
-  }
+  return localStorage.getItem(key);
 }
 
 function save(key, value) {
-  try {
-    const serializedState = JSON.stringify(value);
-    localStorage.setItem(key, serializedState);
-  } catch (err) {
-    console.error('Set state error: ', err);
-  }
+  localStorage.setItem(key, value);
 }
